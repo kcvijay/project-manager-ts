75fbfb11a9f9235ad46a1e976b855e368dd6848f
Vijay KC <vj.kc2007@gmail.com>
2023-01-25 12:39:01 +0200

    eventlistener in button added

--- src/app.js
+++ src/app.js
@@ -7,7 +7,20 @@ var ProjectInput = /** @class */ (function () {
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.attach();
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.attach();
+        this.configure();
     }
+    ProjectInput.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     };
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.querySelector(
       "#project-input"
@@ -19,6 +23,29 @@ class ProjectInput {
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.attach();
+
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.attach();
+    this.configure();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
